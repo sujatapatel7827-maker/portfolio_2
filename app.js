ea6b6bb8413e967e2301857532346206ec61c766
Sujata <sujatapatel7827@gmail.com>
2026-02-20 00:33:33 +0530

    removed duplicate files

--- app.js
+++ app.js
@@ -1,74 +1,37 @@
-<<<<<<< HEAD
 const video1 = document.getElementById('projectVideo1');
 const video2 = document.getElementById('projectVideo2');
 const video3 = document.getElementById('projectVideo3');
 
-// Sidebar elements //
+// Sidebar elements
 const sideBar = document.querySelector('.sidebar');
 const menu = document.querySelector('.menu-icon');
-const closeIcon = document.querySelector('.close-icon')
-
+const closeIcon = document.querySelector('.close-icon');
 
 const hoverSign = document.querySelector('.hover-sign');
 
-const videoList =[video1, video2, video3];
+const videoList = [video1, video2, video3];
 
-videoList.forEach (function(video){
+// Video hover
+videoList.forEach(function(video){
     video.addEventListener("mouseover", function(){
-        video.play()
-        hoverSign.classList.add("active")
-    })
-    video.addEventListener("mouseout", function(){
-    video.pause();
-    hoverSign.classList.remove("active")
-})
-})
+        video.play();
+        hoverSign.classList.add("active");
+    });
 
-// Sidebar elements //
-menu.addEventListener("click", function(){
-    sideBar.classList.remove("close-sidebar")
-    sideBar.classList.add("open-sidebar")
+    video.addEventListener("mouseout", function(){
+        video.pause();
+        hoverSign.classList.remove("active");
+    });
 });
 
+// Sidebar open
+menu.addEventListener("click", function(){
+    sideBar.classList.remove("close-sidebar");
+    sideBar.classList.add("open-sidebar");
+});
+
+// Sidebar close
 closeIcon.addEventListener("click", function(){
     sideBar.classList.remove("open-sidebar");
     sideBar.classList.add("close-sidebar");
-    
-=======
-const video1 = document.getElementById('projectVideo1');
-const video2 = document.getElementById('projectVideo2');
-const video3 = document.getElementById('projectVideo3');
-
-// Sidebar elements //
-const sideBar = document.querySelector('.sidebar');
-const menu = document.querySelector('.menu-icon');
-const closeIcon = document.querySelector('.close-icon')
-
-
-const hoverSign = document.querySelector('.hover-sign');
-
-const videoList =[video1, video2, video3];
-
-videoList.forEach (function(video){
-    video.addEventListener("mouseover", function(){
-        video.play()
-        hoverSign.classList.add("active")
-    })
-    video.addEventListener("mouseout", function(){
-    video.pause();
-    hoverSign.classList.remove("active")
-})
-})
-
-// Sidebar elements //
-menu.addEventListener("click", function(){
-    sideBar.classList.remove("close-sidebar")
-    sideBar.classList.add("open-sidebar")
 });
-
-closeIcon.addEventListener("click", function(){
-    sideBar.classList.remove("open-sidebar");
-    sideBar.classList.add("close-sidebar");
-    
->>>>>>> e4e2c59c65ff1f15efd03aa66a05fe7b6bba8461
-})
